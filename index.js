885811f45516adbd502bda1e3d582f63e74ccc36
Paizutempest <97170451+paizutempest@users.noreply.github.com>
2026-05-24 09:34:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function displayBanner() {
     ██║     ╚██████╔╝██║  ██║███████╗   ╚██████╗╚██████╔╝██║     ██║     ███████╗███████╗
     ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝    ╚═════╝ ╚═════╝ ╚═╝     ╚═╝     ╚══════╝╚══════╝
     FORE COFFEE AUTO SYSTEM
-    By Paizutempest x Yaelanit | Mode: Manual Identity
+    By Paizutempest x Yaelanit
     `));
 }
 
